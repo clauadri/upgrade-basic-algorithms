39791690b9ef07398a1650fd1737bbab21dc6ba6
clauadri <adripa1111@gmail.com>
2022-10-03 21:43:58 +0200

    FEAT:Iteracción #3

---
+++ 3.operadores.js
@@ -0,0 +1,28 @@
+/* 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. */
+
+const num1 = 10*5;
+alert(num1);
+
+/* 1.2 Divide 10 por 2 y muestra el resultado en un alert. */
+
+const num2 = 10/2;
+alert(num2)
+
+/* 1.3 Muestra mediante un alert el resto de dividir 15 por 9. */
+
+alert(15 % 9);
+
+/* 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5. */
+
+const y = 10;
+const z = 5;
+let x 
+console.log(x = y + z);
+
+
+/* 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5. */
+
+
+console.log(x = y * z);
