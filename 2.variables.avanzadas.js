1ecf4901892d75a549a0ca46c6eb47f18d6f3df1
clauadri <adripa1111@gmail.com>
2022-10-04 09:38:12 +0200

    FEAT:fix-errors

--- 2.variables.avanzadas.js
+++ 2.variables.avanzadas.js
@@ -42,8 +42,8 @@ let globalBasePrice = 25000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
-car1.finalPrice = (50000 + globalBasePrice);
-car2.finalPrice = (70000 + globalBasePrice);
+car1.finalPrice = (car1.basePrice + globalBasePrice);
+car2.finalPrice = (car2.basePrice + globalBasePrice);
 
 console.log(car1);
 console.log(car2);
