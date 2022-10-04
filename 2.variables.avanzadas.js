2f8200a1557e1518280a6fe7bf12c7470ffbd993
clauadri <adripa1111@gmail.com>
2022-10-04 09:17:03 +0200

    FEAT:Iteración #2

--- 2.variables.avanzadas.js
+++ 2.variables.avanzadas.js
@@ -1,9 +1,9 @@
 /* 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 const character = {name: 'Jack Sparrow', age: 10}; */
 
-const character = {name: 'Jack Sparrow', age: 25};
+const character = {name: 'Jack Sparrow', age: 10};
 
-
+character.age = 25;
 
 /* 1.2 Declara 3 variables con los nombres y valores siguientes 
 	firstName = 'Jon'; 
